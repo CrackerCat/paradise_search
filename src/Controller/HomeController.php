d8298d04ce943edb1aca2025bd868d66f121c20d
troisa <leondeslilas@gmail.com>
2021-04-01 15:09:34 +0200

    pages home et result

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="id_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/result{my_brand}", name="id_result")
+     */
+    public function result(): Response
+    {
+        return $this->render('home/result.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
