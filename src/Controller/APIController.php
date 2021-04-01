21d6db066d2046cc776800a3b9f888dbf44c2452
troisa <leondeslilas@gmail.com>
2021-04-01 17:34:38 +0200

    modif /API

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -16,42 +16,49 @@ class APIController extends AbstractController
     {
 
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'https://api.opencorporates.com/v0.4/companies/us_va/05501796/statements?api_token=LndrOC38xehzcVPXfIfe');
 
-        $statusCode = $response->getStatusCode();
+        $responseStatements = $httpClient->request('GET', 'https://api.opencorporates.com/v0.4/companies/us_va/05501796/statements?api_token=LndrOC38xehzcVPXfIfe');
+
+        $statusCodeStatements = $responseStatements->getStatusCode();
         // $statusCode = 200
-        dump($statusCode);
-        $content = $response->getContent();
+        //dump($statusCode);
+        $contentStatements = $responseStatements->getContent();
 
-        $resultat = json_decode($content);
+        $resultatStatements = json_decode($contentStatements);
 
-        //$pepitoCompanyName = $resultat->results->company->name;
+        $pepitoCompanySubsNames = $resultatStatements->results->statements;
+        $pepitoCompanySubsCount = $resultatStatements->results->total_count;
 
-        //$pepitoCompanyCountry = $resultat->results->company->jurisdiction_code;
-
-        $pepitoCompanySubsNames = $resultat->results->statements;
-
-        //$pepitoCompanySubsName = [$pepitoCompanySubsNames->statement->properties->subsidiary->name];
-
-        //foreach($pepitoCompanySubsNames as $pepitoCompanySubsName)
-        //{
-        //    $pepitoCompanySubsNames[$pepitoCompanySubsName->statement->properties->subsidiary->name];
-        //}
-
-    
-        //->statement->properties->subsidiary->name;
         // returns the raw content returned by the server (JSON in this case)
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
         //dump($content);
-        $content = $response->toArray();
+        $contentStatements = $responseStatements->toArray();
         // transforms the response JSON content into a PHP array
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
         //dd($pepitoCompanySubsNames);
 
+
+
+        $httpClient = HttpClient::create();
+        
+        $responseCompany = $httpClient->request('GET', 'https://api.opencorporates.com/v0.4/companies/us_va/05501796?api_token=LndrOC38xehzcVPXfIfe');
+
+        $statusCode = $responseCompany->getStatusCode();
+        // $statusCode = 200
+        //dump($statusCode);
+
+        $contentCompany = $responseCompany->getContent();
+
+        $resultatCompany = json_decode($contentCompany);
+        
+        $pepitoCompanyName = $resultatCompany->results->company->name;
+        $pepitoCompanyCountry = $resultatCompany->results->company->jurisdiction_code;
+
         return $this->render('api/index.html.twig', [
-            //'pepito_company_name' => $pepitoCompanyName,
-            //'pepito_company_country' => $pepitoCompanyCountry,
+            'pepito_company_name' => $pepitoCompanyName,
+            'pepito_company_country' => $pepitoCompanyCountry,
             'pepito_company_subs' => $pepitoCompanySubsNames,
+            'pepito_company_subs_count' => $pepitoCompanySubsCount,
         ]);
     }
 }
