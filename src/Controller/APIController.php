9df4df4bf07dbb7f8004af9cca30786e6be10c11
troisa <leondeslilas@gmail.com>
2021-04-01 16:42:41 +0200

    modif twig api

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -16,26 +16,42 @@ class APIController extends AbstractController
     {
 
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'https://api.opencorporates.com/v0.4/companies/us_va/05501796?api_token=LndrOC38xehzcVPXfIfe');
+        $response = $httpClient->request('GET', 'https://api.opencorporates.com/v0.4/companies/us_va/05501796/statements?api_token=LndrOC38xehzcVPXfIfe');
 
         $statusCode = $response->getStatusCode();
         // $statusCode = 200
         dump($statusCode);
         $content = $response->getContent();
 
-         $resultat = json_decode($content);
+        $resultat = json_decode($content);
 
-         echo $resultat->results->company->name ;
+        //$pepitoCompanyName = $resultat->results->company->name;
+
+        //$pepitoCompanyCountry = $resultat->results->company->jurisdiction_code;
+
+        $pepitoCompanySubsNames = $resultat->results->statements;
+
+        //$pepitoCompanySubsName = [$pepitoCompanySubsNames->statement->properties->subsidiary->name];
+
+        //foreach($pepitoCompanySubsNames as $pepitoCompanySubsName)
+        //{
+        //    $pepitoCompanySubsNames[$pepitoCompanySubsName->statement->properties->subsidiary->name];
+        //}
+
+    
+        //->statement->properties->subsidiary->name;
         // returns the raw content returned by the server (JSON in this case)
         // $content = '{"id":521583, "name":"symfony-docs", ...}'
         //dump($content);
         $content = $response->toArray();
         // transforms the response JSON content into a PHP array
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-        dd($content);
+        //dd($pepitoCompanySubsNames);
 
         return $this->render('api/index.html.twig', [
-            'controller_name' => 'APIController',
+            //'pepito_company_name' => $pepitoCompanyName,
+            //'pepito_company_country' => $pepitoCompanyCountry,
+            'pepito_company_subs' => $pepitoCompanySubsNames,
         ]);
     }
 }
