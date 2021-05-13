013ecc23461c69d9b1705c733c854a284c769a1a
troisa <leondeslilas@gmail.com>
2021-05-13 10:57:32 +0200

    Pagination-oriented modification of fetch method

--- src/Controller/MondelezSubsController.php
+++ src/Controller/MondelezSubsController.php
@@ -24,21 +24,74 @@ class MondelezSubsController extends AbstractController
         //list of tax havens, to use as filter for fetched data (as listed by Oxfam here https://www.oxfamfrance.org/communiques-de-presse/liste-noire-des-paradis-fiscaux-lunion-europeenne-doit-faire-pression-sur-les-pays-les-plus-nocifs/)
         $taxhavens = ['Panama', 'Netherlands', 'Switzerland', 'Bermuda', 'Cayman Islands', 'Singapour', 'Irland','Luxemburg', 'Curaçao', 'Hong Kong', 'Cyprus', 'Bahamas', 'Jersey', 'Barbados', 'Mauritius', 'British Virgin Islands'];
         
+        //
+        //TEMPORARY SOLUTION BEFORE IMPLMENTING CORRECT PAGINATION WITH CURRENTPAGE TRANSFERRED FROM AGENDA COMPONENT VIA GET
+        //
+        
         //filtering the fetched data from database with tax haven list and ordering them alphabetically
-        $subsresult = $repository->findBy(array(
+        $subsresultOne = $repository->findBy(array(
             'subscountry' => $taxhavens
         ), array(
             'subsname' => 'Desc',
-        ));
+        ), 10, 0);
+
+        //creation of an array to put the data in
+        $subsendresultsOne = [];
+        
+        //feeding the newly created array with all the data from master array
+        for ($i = 0; $i <= count($subsresultOne)-1; $i++) {
+            $subspush = array('subsname' => $subsresultOne[$i]->getSubsName(), 'subscountry' => $subsresultOne[$i]->getSubsCountry());
+            array_push($subsendresultsOne, $subspush);
+        } 
+
+
+
+        $subsresultTwo = $repository->findBy(array(
+            'subscountry' => $taxhavens
+        ), array(
+            'subsname' => 'Desc',
+        ), 10, 10);
+
+        //creation of an array to put the data in
+        $subsendresultsTwo = [];
+        
+        //feeding the newly created array with all the data from master array
+        for ($i = 0; $i <= count($subsresultTwo)-1; $i++) {
+            $subspush = array('subsname' => $subsresultTwo[$i]->getSubsName(), 'subscountry' => $subsresultTwo[$i]->getSubsCountry());
+            array_push($subsendresultsTwo, $subspush);
+        } 
+
+
+        $subsresultThree = $repository->findBy(array(
+            'subscountry' => $taxhavens
+        ), array(
+            'subsname' => 'Desc',
+        ), 10, 20);
+
+        //creation of an array to put the data in
+        $subsendresultsThree = [];
+        
+        //feeding the newly created array with all the data from master array
+        for ($i = 0; $i <= count($subsresultThree)-1; $i++) {
+            $subspush = array('subsname' => $subsresultThree[$i]->getSubsName(), 'subscountry' => $subsresultThree[$i]->getSubsCountry());
+            array_push($subsendresultsThree, $subspush);
+        } 
+
+
+        $subsresultFour = $repository->findBy(array(
+            'subscountry' => $taxhavens
+        ), array(
+            'subsname' => 'Desc',
+        ), 10, 30);
         //dd($subsresult);
 
         //creation of an array to put the data in
-        $subsendresults = [];
+        $subsendresultsFour = [];
         
         //feeding the newly created array with all the data from master array
-        for ($i = 0; $i <= count($subsresult)-1; $i++) {
-            $subspush = array('subsname' => $subsresult[$i]->getSubsName(), 'subscountry' => $subsresult[$i]->getSubsCountry());
-            array_push($subsendresults, $subspush);
+        for ($i = 0; $i <= count($subsresultFour)-1; $i++) {
+            $subspush = array('subsname' => $subsresultFour[$i]->getSubsName(), 'subscountry' => $subsresultFour[$i]->getSubsCountry());
+            array_push($subsendresultsFour, $subspush);
         } 
         //dd($subsendresults);
 
@@ -49,7 +102,10 @@ class MondelezSubsController extends AbstractController
         
         //sending the data fetched from database to the url /mondelez/subs
         return new JsonResponse([
-            'Mondelez_subs' => $subsendresults
+            'Mondelez_subs_one' => $subsendresultsOne,
+            'Mondelez_subs_two' => $subsendresultsTwo,
+            'Mondelez_subs_three' => $subsendresultsThree,
+            'Mondelez_subs_four' => $subsendresultsFour
         ]);
     }
 }
